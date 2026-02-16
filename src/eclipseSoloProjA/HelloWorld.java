50f2af65fc486b0dead65c6b6042282bae908c51
bcwei <bcwei@164.76.44.164>
2026-02-16 14:18:30 -0500

    Trying to get the ss for the lab

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -5,7 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
 		System.out.println();
 		System.out.println("Programer 2 did this, Programmer 1 did this");
 	}
