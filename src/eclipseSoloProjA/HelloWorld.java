13f7d80e8d580ba5b15029c7eadfa7ef1ae0befc
bcwei <bcwei@164.76.44.164>
2026-02-16 14:18:08 -0500

    Fixed the merge issue

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
 		System.out.println();
 		System.out.println("Programer 2 did this, Programmer 1 did this");
 	}
